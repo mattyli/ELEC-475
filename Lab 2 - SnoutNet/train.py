62848176f24850f568513874697dab67d36fa991
Matthew Li <matthew.li@queensu.ca>
2024-10-05 13:52:24 -0400

    patched issue where validation loss was not being appended to list. added cmd line arg for dropout rate.

--- Lab 2 - SnoutNet/train.py
+++ Lab 2 - SnoutNet/train.py
@@ -8,7 +8,7 @@ import datetime
 import argparse
 from torch.utils.data import DataLoader
 from SnoutDataset import SnoutDataset
-from model import SnoutNet
+from model import SnoutNet, EuclideanLoss
 from pathlib import Path
 from torchinfo import summary
 from tqdm import tqdm
@@ -28,9 +28,6 @@ KERNEL_SIZE = 3                                                     # kernel siz
 STRIDE = 2                                                          # stride for maxpool and conv
 PADDING = 1                                                         # padding for maxpool and conv
 DROPOUT_P = 0.0                                                     # dropout probability
-IMAGE_FOLDER = Path("images/")
-TRAIN_PATH = Path("train_noses.txt")
-TEST_PATH = Path("test_noses.txt")
 
 # Play around with this
 def init_weights(layer: nn.Module):
@@ -40,8 +37,9 @@ def init_weights(layer: nn.Module):
         layer (nn.Module): Network layer
     """
     if isinstance(layer, nn.Conv2d):
-        nn.init.normal_(layer.weight, mean=0, std=0.01)
-        nn.init.constant_(layer.bias, 0)
+        # nn.init.normal_(layer.weight, mean=0.0, std=0.01)
+        nn.init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
+        nn.init.constant_(layer.bias, 0.0)
     if isinstance(layer, nn.Linear):
         torch.nn.init.xavier_uniform_(layer.weight)
         layer.bias.data.fill_(0.01)
@@ -58,20 +56,19 @@ def train(n_epochs,
           save_file,
           plot_file
           )->None:
-    print("training model...")
 
     losses_train = []
     losses_validation = []
     for epoch in tqdm(range(n_epochs)):
         print(f"Epoch: {epoch+1}")
         loss_train = 0.0
-        validation_loss = 0.0
         
         # train loop
         model.train()
         for batch in train_loader:
             images, centers = batch['image'], batch['center']       # load images and ground truth (labelled centers)
-            images = images.to(device=device)                       
+            images = images.to(device=device).float()
+            centers = centers.to(device=device).float()                          
             predicted_centers = model(images)                       
             loss = loss_fn(predicted_centers, centers)              
 
@@ -82,11 +79,15 @@ def train(n_epochs,
 
         # validation loop
         model.eval()
-        for validation_batch in validation_loader:
-            validation_images, validation_centers = validation_batch['image'], validation_batch['center']
-            validation_images = validation_images.to(device=device)
-            validation_pred = model(validation_images)
-            loss = loss_fn(validation_pred, validation_centers)
+        validation_loss = 0.0
+        with torch.no_grad():
+            for validation_batch in tqdm(validation_loader, leave=False):
+                validation_images, validation_centers = validation_batch['image'], validation_batch['center']
+                validation_images = validation_images.to(device=device).float()
+                validation_centers = validation_centers.to(device=device).float()
+                validation_pred = model(validation_images)
+                loss = loss_fn(validation_pred, validation_centers)
+                validation_loss += loss.item()
 
         scheduler.step(loss_train)
         epoch_train_loss = loss_train/len(train_loader)
@@ -94,6 +95,7 @@ def train(n_epochs,
         losses_train += [epoch_train_loss]
         losses_validation += [epoch_validation_loss]
         print(f"{datetime.datetime.now()} Epoch: {epoch+1}, Training Loss: {epoch_train_loss}, Validation Loss: {epoch_validation_loss}")
+        #   break   # debug
     
     # moved to outside the loop, don't need to redraw image every epoch
     if save_file:
@@ -115,6 +117,7 @@ def main():
     plot_file = "loss.png"
     n_epochs = N_EPOCHS
     batch_size = BATCH_SIZE
+    dropout = DROPOUT_P
 
     print('running main ...')
 
@@ -124,6 +127,7 @@ def main():
     argParser.add_argument('-e', metavar='epochs', type=int, help='# of epochs [30]')
     argParser.add_argument('-b', metavar='batch size', type=int, help='batch size [32]')
     argParser.add_argument('-p', metavar='plot', type=str, help='output loss plot file (.png)')
+    argParser.add_argument('-d', metavar='dropout rate', type=float, help='dropout rate')
 
     args = argParser.parse_args()
 
@@ -135,6 +139,8 @@ def main():
         batch_size = args.b
     if args.p != None:
         plot_file = args.p
+    if args.d != None and args.d <= 1.0 and args.d >= 0.0:
+        dropout = args.d
 
     print('\t\tn epochs = ', n_epochs)
     print('\t\tbatch size = ', batch_size)
@@ -144,21 +150,28 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('\t\tusing device ', device)
 
-    model = SnoutNet(kernel_size=KERNEL_SIZE, stride=STRIDE, padding=PADDING, dropout=DROPOUT_P)
-    model.to(device)
+    model = SnoutNet(kernel_size=KERNEL_SIZE, stride=STRIDE, padding=PADDING, dropout=dropout)
+    model.to(device=device)
     model.apply(init_weights)
-    summary(model, model.input_shape)
+    summary(model)
     transform_pipeline = transforms.Compose([snoutTransforms.RescaleImage(IMAGE_SIZE), snoutTransforms.ToTensor()])
 
-    train_set = SnoutDataset(label_path="train_noses.txt", image_folder="images-original/", transform=transform_pipeline)
+    print("Fetching training set...")
+    train_set = SnoutDataset(label_path="train_noses.txt", image_folder="images/", transform=transform_pipeline)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
-    validation_set = SnoutDataset(label_path="test_noses.txt", image_folder="images-original/", transform=transform_pipeline)
+    print("Fetching validation set...")
+    validation_set = SnoutDataset(label_path="test_noses.txt", image_folder="images/", transform=transform_pipeline)
     validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
 
     optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+    print("optimizer created...")
+
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min')
+    print("scheduler created...")
     loss_fn = nn.MSELoss(size_average=None, reduce=None, reduction='mean')
+    print("loss function created...")
+    print('Entering training loop...')
 
     train(n_epochs=n_epochs,
           optimizer=optimizer,
@@ -172,5 +185,5 @@ def main():
           plot_file=plot_file
           )
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
